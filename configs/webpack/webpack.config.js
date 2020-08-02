00ee43326922449c994c829f8566b8c66fce721e
HMhugomeireles <meireles.hmm@gmail.com>
2020-08-03 00:25:03 +0100

    remove dist folder

--- configs/webpack/webpack.config.js
+++ configs/webpack/webpack.config.js
@@ -1,5 +1,4 @@
 const path = require('path');
-const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackNodeExternals = require('webpack-node-externals');
 
@@ -48,7 +47,7 @@ const backendConfiguration = {
   },
   devtool: 'source-map',
   resolve: {
-    extensions: ['.ts', '.tsx', '.js']
+    extensions: ['.ts', '.tsx']
   },
   module: {
     rules: [
